56530b1a62c2af99e281bd187ca76a7fccc0518a
Anita Lee <anita.lee@gmail.com>
2022-03-11 19:48:20 -0500

    average-pair.js updated

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,25 @@
+//create two pointers 
+//initialize (left [0] & right [length -1])
+//while loop ( value < ((left + right)/2) ) left++
+//else right--
+//if avg === (left + right)/2 => return true
+//if none, return false
+
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+function averagePair(arr, avg) {
+    if (arr === undefined) return false; 
+
+    let left = arr[0]; 
+    let right = arr[arr.length - 1]; 
+    let testAvg = parseFloat(left + right)/2; 
+    console.log(testAvg); 
+
+    while (left < right){
+        if (avg === testAvg) return true; 
+        else if (testAvg < avg) left++;
+        else right--; 
+        console.log(testAvg); 
+    }
+    return false; 
 }
